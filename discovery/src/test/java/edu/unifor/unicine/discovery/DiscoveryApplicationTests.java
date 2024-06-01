8c9f8bb91db0926e63db745759f713af87230125
clys-man <clysmanalves@gmail.com>
2024-06-01 16:31:06 -0300

    feat: add discovery and gateway setup

---
+++ discovery/src/test/java/edu/unifor/unicine/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unifor.unicine.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
