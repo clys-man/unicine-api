1493dc2b71c91e461970c1066ecc61fdade340fb
Fernandasacosta <107959631+Fernandasacosta@users.noreply.github.com>
2024-06-01 22:35:58 -0300

    Create microservice room

--- core/src/main/java/edu/unifor/unicine/core/repository/RoomRepository.java
+++ core/src/main/java/edu/unifor/unicine/core/repository/RoomRepository.java
@@ -1,9 +1,18 @@
 package edu.unifor.unicine.core.repository;
 
 import edu.unifor.unicine.core.model.entity.Room;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
+	
+	public Room findByNumber(int numer);
+	
+	public List<Room> findByTypeContains(String type);
+	
+	public List<Room> findByLocationContains(String location);
 }
